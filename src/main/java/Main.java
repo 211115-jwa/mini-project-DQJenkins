130abf14cb7ff83dca93312b4b1fb854d32166dc
Dana Jenkins <94149057+DQJenkins@users.noreply.github.com>
2021-11-29 07:56:03 -0800

    WIP collection.html and main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,39 @@
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import io.javalin.Javalin;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// creating the Javalin object that we can use
+		Javalin app = Javalin.create();
+		
+		// starting the Javalin app
+		// (opening up to receive requests)
+		app.start();
+		ArrayList<String> collection = new ArrayList<String>();
+		// if we get a request with the "POST" method
+		// and the URI ends with "/collect"
+		app.post("/collect", ctx -> {
+			// get the item from the request body
+			String item = ctx.body().replaceFirst("item=", "");
+			//ctx.result(item);
+			collection.add(item);
+		});
+		
+		app.get("/display", ctx-> {
+			int count = 1;
+			String responseText = "";
+			for(String item: collection)
+			{
+				responseText += ("Item " + count + ": " + item + "\n");
+				count++;
+			}		
+			ctx.result(responseText);
+		});
 	}
+
 
 }
